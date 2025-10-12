1f53aecd8262eb20c814fcdeb3ae92ef14d6d2b8
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2025-10-12 13:35:48 -0300

    Create acao.php

---
+++ php/crud/acao.php
@@ -0,0 +1,66 @@
+<?php //ações gerais do crud
+
+// "conecta" ao conectar.php (como se inserisse o conteúdo de um arquivo PHP dentro de outro)
+require("conectar.php");
+//tratamento de erro de conexão ($conn é um dos padrões da linguagem
+if ($conn->connect_error) {
+  die("Conexão falhou: " . $conn->connect_error);
+}
+
+//ação de inserir dados no banco
+if (isset($_POST['enviar'])) {
+  //pega elementos com base no id do index
+  //o "trim" é para remover espaços no início e fim da string caso tenha
+  $nome = trim($_POST['nome']);
+  $nascimento = trim($_POST['nascimento']);
+  $renda = trim($_POST['renda']);
+  
+  //valida se os valores de "renda" estão dentro de um certo escopo
+  //"/^" e "/$" indicam início e fim de string
+  //"[0-9]" indica que são ceitos apenas "strings" que sejam números (qualquer número que contenha valor de 0 a 9)
+  //esse "+"após o último gurpo mencionado quer dizer que pode ter um ou mais dígitos
+  //o "[.,][0-9]+" quer dizer que dentro desse grupo em parêntesis haverá um pontou ou vírgula seguido de um ou mais números
+  //o "?" ao final diz que esse grupo em parênteses é opcional, se não houvesse parênteses o "?" afetaria apenas o "+" que é o mais próximo
+  if (!preg_match('/^[0-9]+([.,][0-9]+)?$/', $renda)) {
+      echo "Erro: A renda deve conter apenas valores numéricos válidos.";
+      exit;
+  }
+  
+  $data_formatada = date("Y-m-d", strtotime($nascimento));
+  $renda_formatada = str_replace(',', '.', $renda);
+  $renda_formatada = floatval($renda_formatada);
+  
+  $sql = $conn->prepare("INSERT INTO usuarios (nome, renda, nascimento) VALUES (?, ?, ?)");
+  $sql->bind_param("sds", $nome, $renda_formatada, $data_formatada);
+  
+  if ($sql->execute() === TRUE) {
+      header("location: index.php");
+  } else {
+      echo "Erro: " . $conn->error;
+  }
+
+  $sql->close();
+}
+
+//ação de excluir dados do banco
+if (isset($_POST['enviar_excluir'])) {
+  $excluir = $_POST['excluir'];
+  
+  //prepare faz os dados serem tratados apenas como valores, não comandos assim evitando sql injection
+  //""s", excluir" indica para usar a variável excluir como uma string
+  $sql = $conn->prepare("DELETE FROM usuarios WHERE nome = ?");
+  $sql->bind_param("s", $excluir);
+  
+  if ($sql->execute() === TRUE) {
+      header("location: index.php");
+  } else {
+      echo "Erro: " . $conn->error;
+  }
+
+  $sql->close();
+}
+
+
+$conn->close();
+
+?>
