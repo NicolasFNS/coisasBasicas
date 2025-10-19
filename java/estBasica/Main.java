fde781bdd3acd08e3a7d82a4188574a7b03c966e
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2025-10-19 11:34:26 -0300

    Create basic structure for Java Main class
    Added a basic structure for a Java main class with a main method.

---
+++ java/estBasica/Main.java
@@ -0,0 +1,12 @@
+//estrututra base de um main java
+
+import java.util.Scanner;//a maioria das vezes esse import é necessário
+
+public class Main {
+    public static void main(String[] args) { //geralmente é nessa função que acontece a interação do código com usuário
+      
+    }
+
+  //outras funções
+  
+}
