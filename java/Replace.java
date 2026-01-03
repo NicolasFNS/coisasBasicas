1254dd8384f7fd9a2c9afa352a58b578b0ce02cd
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-03 15:03:45 -0300

    Add FakeBinary class with fakeBin method
    Implement fakeBin method to convert digits based on value.

---
+++ java/Replace.java
@@ -0,0 +1,36 @@
+/*
+desafio básico do codewars:
+"Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string."
+*/
+public class FakeBinary {
+  public static String fakeBin(String numberString) {
+    
+    String novo = numberString.replaceAll("[0-4]","0").replaceAll("[5-9]", "1");
+    
+    return novo;
+  }
+}
+
+/* acima é um exemplo mais simplificado, mas para prática geral pode ser melhor conhecer o processo
+
+public class FakeBinary {
+    public static String fakeBin(String numberString) {
+        StringBuilder result = new StringBuilder(); //construtor da nova string (para poder usar o append)
+
+        for (int i = 0; i < numberString.length(); i++) {
+        
+            char digit = numberString.charAt(i); //pega o dígito
+            int num = Character.getNumericValue(digit); //converte para número
+
+            if (num < 5) {
+                result.append('0');
+            } else {
+                result.append('1');
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+*/
