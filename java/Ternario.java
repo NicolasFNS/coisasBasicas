8f5977a1e50ecf5bc051e52a14b3ec7902115314
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-03 15:21:52 -0300

    Using ternary operator

---
+++ java/Ternario.java
@@ -0,0 +1,21 @@
+/*
+  operação ternária, apenas uma forma de simplificação do if else, no exemplo logo abaixo segue e estrutura:
+  if(c[i] < '5')
+    c[i]=0;
+  else{
+    c[i]=1;
+  }
+*/
+
+public class Ternario {
+  public static String tern() {
+    String numeros = "09091728307108237097374356345";
+    char c[] = numeros.toCharArray();
+    
+    for (int i = 0; i < c.length; i++){
+      c[i] = c[i] < '5' ? '0' : '1';
+    }
+    
+    return new String(c);
+  }
+}
