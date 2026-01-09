c6b1c2770fc6996999167666212be74fad996922
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-09 12:46:59 -0300

    Enhance Type.java with type conversion examples
    Added various examples of type conversions and string manipulations.

---
+++ java/Type.java
@@ -0,0 +1,37 @@
+//double -> int
+double a1 = 9.7;
+int a2 = (int) a1; // double -> int (perde a parte decimal)
+
+//varios -> String
+Integer b1 = 10;
+String b2 = b1.toString();
+
+int b3 = 10;
+String b4 = String.valueOf(b3);
+String b5 = b3 + "";//mesmo resultado que a linha anterior mas com concatenação
+String b6 = Integer.toString(b3);
+
+double b9 = 3.14159;
+String b10 = String.format("%.2f", b9); //o "%.2f" limita as casas decimais para apenas duas, retornando "3.14"
+
+String b11 = "Java";
+char[] b12 = b11.toCharArray();
+String b13 = new String(b12);
+
+boolean b14 = Boolean.parseBoolean("true");
+String b15 = Boolean.toString(b14);
+
+LocalDate b16 = LocalDate.parse("2024-01-09");
+String b17 = b16.toString();
+
+DateFormat b18 = new SimpleDateFormat("dd/MM/yyyy");
+String b19 = b18.format(new Date());//resulta numa string com a data e hora atuais do sistema
+
+//objetos para tipos
+Object c1 = "texto";
+String c2 = (String) c1;
+
+//array -> list
+String[] arr = {"a", "b", "c"};
+List<String> list = Arrays.asList(arr);
+String[] arr2 = list.toArray(new String[0]);
