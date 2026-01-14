ffc6973c3c4ac7e23bb9881de8c78e7a481ae506
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-14 16:26:11 -0300

    Add function to convert string to uppercase

---
+++ java/ToUpperCase.java
@@ -0,0 +1,8 @@
+//desafio pediu uma função que retorna a forma maiuscula de uma string
+
+class Upper {
+  public static String makeUpperCase(String str) {
+    return str.toUpperCase();
+  }
+}
+//apenas isso já vai fazer tudo
