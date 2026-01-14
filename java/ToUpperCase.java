ae4319cc9db36a4e060081ced70f1292552197f4
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-14 16:49:53 -0300

    Update ToUpperCase.java

--- java/ToUpperCase.java
+++ java/ToUpperCase.java
@@ -1,8 +1,19 @@
 //desafio pediu uma função que retorna a forma maiuscula de uma string
 
 class Upper {
-  public static String makeUpperCase(String str) {
+  
+  public static String makeUpperCase(String str) {//tudo maiúsculo
     return str.toUpperCase();
   }
+
+  public static String capitalizeFirstTrim(String s) {//coloca uma letra maiúscula no começo
+    if (s == null) return null;//evita NullPointerException
+
+    s = s.trim();//remove espaço em branco do começo e fim
+    if (s.isEmpty()) return s;//para caso não ter conteúdo
+
+    return s.substring(0, 1).toUpperCase() + s.substring(1);
+  }
+  
 }
-//apenas isso já vai fazer tudo
+
