ac99de12f30e7b35be834a497f79b1ceeb05c32f
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-22 13:15:12 -0300

    Create charAt.java

---
+++ java/charAt.java
@@ -0,0 +1,17 @@
+public class Kata {//desafio que retorna true se o "ending" estiver no fim de "str"
+  public static boolean solution(String str, String ending) {
+    if (ending.length() > str.length()) {
+      return false;
+    }
+    
+    int leng = str.length() - 1;
+    
+    for (int i = ending.length() - 1; i >= 0; i--) {
+      if (ending.charAt(i) != str.charAt(leng)) {
+        return false;
+      }
+      leng--;
+    }
+    return true;
+  }
+}
