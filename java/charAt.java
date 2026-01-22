7e4f9f8e3fa3a354a5eabc8bc18a955ee1e185dc
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-01-22 13:17:35 -0300

    Update charAt.java

--- java/charAt.java
+++ java/charAt.java
@@ -1,6 +1,6 @@
 public class Kata {//desafio que retorna true se o "ending" estiver no fim de "str"
   public static boolean solution(String str, String ending) {
-    if (ending.length() > str.length()) {
+    if (ending.length() > str.length()) {//o desafio mostrou o erro "String index out of range: -1", esse if evita que o "ending" seja maior que "str"
       return false;
     }
     
