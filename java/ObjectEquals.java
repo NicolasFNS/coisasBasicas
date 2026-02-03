889e20b90a209414f3c9bca2c3477c56d55e69af
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-02-03 14:48:26 -0300

    Enhance ObjectEquals.java with comments and alternatives
    Added comments explaining the purpose of the contains method and alternative solutions.

--- java/ObjectEquals.java
+++ java/ObjectEquals.java
@@ -1,5 +1,3 @@
-//comparação de obejtos, também dá para usar o conteúdo do arquivo FindStringInObject.java no lugar
-
 public static boolean contains(Object[] array, Object value) {
     //if (array == null) return false; para não comparar nada nulo
     for (Object element : array) {
@@ -7,3 +5,15 @@ public static boolean contains(Object[] array, Object value) {
     }
     return false;
 }
+
+/*
+o desafio pedia para verificar se um objeto expecífico existia na lista de arrays, 
+também dá para usar o conteúdo do arquivo FindStringInObject.java no lugar(o que eu fiz), ou até:
+
+public class Solution {
+    public static boolean check(Object[] a, Object x) {
+        return java.util.Arrays.asList(a).contains(x);
+    }
+}
+
+*/
