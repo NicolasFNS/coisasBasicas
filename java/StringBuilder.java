358084c4438aae16666576ecbf0cda922ef44e57
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-02-08 16:20:38 -0300

    Add string reversal solution using StringBuilder
    Implement string reversal using StringBuilder in Java.

---
+++ java/StringBuilder.java
@@ -0,0 +1,28 @@
+/*
+como o nome ja sugere, ele serve para construir strings
+mas serve para usar a construção de string com lógicas incluídas
+como por exemplo um algoritmo que inverte strings
+*/
+
+//o que eu fiz no desafio do codewars
+public class Kata {
+  public static String solution(String str) {
+    StringBuilder letra = new StringBuilder();
+    
+    for(int i = str.length() - 1; i >= 0; i--){
+      letra.append(str.charAt(i));
+    }
+    
+    return letra.toString();
+  }
+}
+
+/*o que eu poderia ter feito
+public class Kata {
+
+  public static String solution(String str) {
+    return new StringBuilder(str).reverse().toString();
+  }
+
+}
+*/
