f0a929d201e39cd701ba5ec921292f71f23b7910
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-02-09 15:43:45 -0300

    Refactor contains method into ObjectEquals class
    Refactor contains method to be part of ObjectEquals class and add return statement.

--- java/ObjectEquals.java
+++ java/ObjectEquals.java
@@ -1,16 +1,18 @@
-public static boolean contains(Object[] array, Object value) {
-    //if (array == null) return false; para não comparar nada nulo
-    for (Object element : array) {
-        if (Objects.equals(element, value)) return true;
+public class ObjectEquals {
+    public static boolean contains(Object[] array, Object value) {
+        //se quiser use um if (array == null) return false; para não comparar nada nulo
+        for (Object element : array) {
+            if (Objects.equals(element, value)) return true;
+        }
+        return false;
     }
-    return false;
 }
 
 /*
 o desafio pedia para verificar se um objeto expecífico existia na lista de arrays, 
 também dá para usar o conteúdo do arquivo FindStringInObject.java no lugar(o que eu fiz), ou até:
 
-public class Solution {
+public class ObjectEquals {
     public static boolean check(Object[] a, Object x) {
         return java.util.Arrays.asList(a).contains(x);
     }
