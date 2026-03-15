372f6ff6b44ae288db211cfe7203f4744c8e75aa
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-03-15 14:15:13 -0300

    Implement snail function to convert 2D array to 1D

---
+++ java/ListToArray.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Snail {
+
+    public static int[] snail(int[][] array) {
+        if (array == null || array.length == 0 || array[0].length == 0){
+            return new int[0];
+        }
+      
+        List<Integer> result = new ArrayList<>();
+
+        int minY = 0;
+        int maxY = array.length - 1;
+        int minX = 0;
+        int maxX = array[0].length - 1;
+
+        while (minY <= maxY && minX <= maxX) {
+          
+            for (int i = minX; i <= maxX; i++) {
+                result.add(array[minY][i]);
+            }
+            minY++;
+
+            for (int i = minY; i <= maxY; i++) {
+                result.add(array[i][maxX]);
+            }
+            maxX--;
+
+            if (minY <= maxY) {
+                for (int i = maxX; i >= minX; i--) {
+                    result.add(array[maxY][i]);
+                }
+                maxY--;
+            }
+
+            if (minX <= maxX) {
+                for (int i = maxY; i >= minY; i--) {
+                    result.add(array[i][minX]);
+                }
+                minX++;
+            }
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}   
