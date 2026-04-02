78ab133146b385a30b2a61a5f40f2f62cee2b9f7
NicolasFNS <99752258+NicolasFNS@users.noreply.github.com>
2026-04-02 14:52:32 -0300

    Add comments to SQL select statement
    Added comments explaining the SQL select operation for clarity.

---
+++ sql/manipulaSelect.sql
@@ -0,0 +1,7 @@
+SELECT 
+  REPEAT(name, 3) AS name,
+  REVERSE(characteristics) AS characteristics
+FROM monsters
+
+/* o desafio eraa fazer um select onde o nome é a string original repetida três vezes (não adicione espaços)
+e as características são as strings originais ao contrário (por exemplo, 'abc, def, ghi' se torna 'ihg,fed,cba')*/
